b8b5fbc850711f3c57887961d68045e75d411db2
Adithyan M.S <adithyan2ms@gmail.com>
2022-04-15 22:03:00 +0530

    download.ejs setuping..

---
+++ routes/show.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const File = require('../models/file');
+
+router.get('/:uuid', async(req,res)=>{
+    try{
+        const file = await File.findOne({uuid: req.params.uuid});
+        if (!file) {
+            return res.render('download',{error: 'Link has been Expired'});
+        }
+
+        return res.render('download',{
+            uuid: file.uuid,
+            fileName:file.fileName,
+            fileSize: file.size,
+            download:`${process.env.APP_BASE_URL}/files/download${file.uuid}`
+        })
+    }catch(err){
+        return res.render('download',{error: 'Something went Wrong'});
+    }
+});
+
+
+
+module.exports= router;
